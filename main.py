679fcfe23d2d33cbd31283130e4051ee9f789a06
yurin <y.nalesso@reply.it>
2020-03-30 01:14:49 +0200

    Log folder

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Created on Sat Jun  1 00:31:32 2019
 @author: joynt
 """
 import os
+from pathlib import Path
 
 if os.name != 'nt':
     import daemon
@@ -20,8 +21,10 @@ if __name__ == "__main__":
     if os.name == 'nt':
         telegram.start()
     else:
-        pidfile = pidfile.TimeoutPIDLockFile('covidDaemon_{}.pid'.format(datetime.now().ctime()))
-        err = open('err_{}.txt'.format(datetime.now().ctime()), 'w+')
-        out = open('out_{}.txt'.format(datetime.now().ctime()), 'w+')
+        log = Path("log/")
+        log.mkdir(parents=True, exist_ok=True)
+        pidfile = pidfile.TimeoutPIDLockFile(log / 'covidDaemon_{}.pid'.format(datetime.now().ctime()))
+        err = open(log / 'err_{}.txt'.format(datetime.now().ctime()), 'w+')
+        out = open(log / 'out_{}.txt'.format(datetime.now().ctime()), 'w+')
         with daemon.DaemonContext(pidfile=pidfile, working_directory='./', stderr=err, stdout=out):
             telegram.start()
